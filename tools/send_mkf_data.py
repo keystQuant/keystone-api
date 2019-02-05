7ba3425e4911f43264d4f922741a8dffa3e615c4
MH-Lee <lmhoon012@gmail.com>
2019-02-06 02:45:15 +0900

    data send 완성

--- tools/send_mkf_data.py
+++ tools/send_mkf_data.py
@@ -1,12 +1,12 @@
 '''
 Date : 7/23/2018
 author : robby
-sprint : 3rd
-description : ppt 데이터 데이터 베이스로 전송
+description : send db mkfdata
 '''
 
 import pandas as pd
 import os, sys, glob
+from datetime import datetime
 
 
 start_path = os.getcwd()
@@ -20,31 +20,29 @@ application = get_wsgi_application()
 
 from stocks.models import MkfGro, MkfVal
 
-data_list = os.listdir('../data')
+today = datetime.today().strftime("%Y%m%d")
+data_list = os.listdir('./data')
 print(os.getcwd())
-
-csv_data = pd.read_excel('../data/' + data_list[0])
-print(csv_data.columns)
-print(len(csv_data.columns))
-print(csv_data.ix[1])
 
 def data_import():
     gro_list = []
     val_list = []
     for excel_file in data_list:
         print(excel_file)
-        mkf_data = pd.read_excel('../data/' + excel_file)
+        mkf_data = pd.read_csv('./data/' + excel_file, encoding='CP949')
         for i in range(mkf_data.shape[0]):
             print(mkf_data.iloc[i]['name'])
-            date = mkf_data.iloc[i]['date']
-            code = mkf_data.iloc[i]['code']
+            date = today
+            code = mkf_data.iloc[i]['code'][1:]
+            print(code)
             name = mkf_data.iloc[i]['name']
-            type = mkf_data.iloc[i]['type']
             sector = mkf_data.iloc[i]['sector']
             if excel_file == 'mkfgro.csv':
+                type = '성장주'
                 mkf_gro_obj = MkfGro(date=date, code=code, name=name, type=type, sector=sector)
                 gro_list.append(mkf_gro_obj)
             elif excel_file == 'mkfval.csv':
+                type = '가치주'
                 mkf_val_obj = MkfVal(date=date, code=code, name=name, type=type, sector=sector)
                 val_list.append(mkf_val_obj)
             else:
